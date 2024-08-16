cc5b21f238673ad2d8c877e69d42b75d6a8624e0
avallejo <avallejofornes@gmail.com>
2024-08-16 13:50:02 +0200

    improve note editor page

--- src/modules/notes/pages/edit/edit.tsx
+++ src/modules/notes/pages/edit/edit.tsx
@@ -1,5 +1,8 @@
 import { api } from "@/trpc/server";
 import { Editor } from "@/modules/notes/components/editor";
+import { Suspense } from "react";
+import { notFound } from "next/navigation";
+import { EditTitleButton } from "../../components/editTitlebutton";
 
 interface NoteEditorProps {
   id: number;
@@ -7,6 +10,20 @@ interface NoteEditorProps {
 
 export async function NoteEditor({ id }: NoteEditorProps) {
   const note = await api.notes.getById({ id });
-  console.log(note);
-  return <>{note && <Editor note={note} />}</>;
+  if (!note) {
+    return notFound();
+  }
+  return (
+    <main className="h-[90vh]">
+      <section className="container mx-auto h-full px-4 py-12 md:px-6">
+        <div className="mb-8 flex items-center gap-2 text-2xl font-bold">
+          <h2>Edit Note: {note.title}</h2>
+          <EditTitleButton noteId={note.id} />
+        </div>
+        <Suspense fallback="Loading...">
+          <Editor note={note} />
+        </Suspense>
+      </section>
+    </main>
+  );
 }
