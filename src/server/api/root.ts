fab424bf1542ede11432e76bd0990a9165bad7c7
avallejo <avallejofornes@gmail.com>
2024-08-16 09:59:26 +0200

    remove t3 template components

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "@/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { notesRouter } from "./routers/notes";
 
@@ -8,7 +7,6 @@ import { notesRouter } from "./routers/notes";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   notes: notesRouter,
 });
 
