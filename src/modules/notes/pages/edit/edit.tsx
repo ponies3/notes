bf3fd266653c0e6c2002f9bee30a4a0e081b7c97
avallejo <avallejofornes@gmail.com>
2024-08-16 22:24:37 +0200

    fix large title in edit page

--- src/modules/notes/pages/edit/edit.tsx
+++ src/modules/notes/pages/edit/edit.tsx
@@ -17,9 +17,9 @@ export async function NoteEditor({ id }: NoteEditorProps) {
   return (
     <main className="h-[90vh]">
       <Header>
-        <div className="container relative flex w-full items-center gap-2 pl-20 text-4xl font-bold">
+        <div className="relative flex w-full items-center justify-center gap-2 pl-20 text-4xl font-bold">
           <BackButton className="absolute left-0" href="/notes" />
-          <div className="flex w-full flex-row items-center justify-center gap-2">
+          <div className="flex w-full flex-row items-center justify-center gap-2 md:max-w-[600px] xl:max-w-[900px] 2xl:max-w-[1100px]">
             <h2 className="truncate">{note.title}</h2>
             <EditTitleButton noteId={note.id} />
           </div>
