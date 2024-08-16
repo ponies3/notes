1fefd49f1956ae22050441924572b8ad0e4a21df
avallejo <avallejofornes@gmail.com>
2024-08-16 19:47:47 +0200

    fix NoteCard when title is to large

--- src/modules/notes/components/noteCard.tsx
+++ src/modules/notes/components/noteCard.tsx
@@ -13,7 +13,7 @@ export function NoteCard({ note }: NoteCardProps) {
           {/* preview */}
         </div>
         <CardContent className="p-4">
-          <h3 className="text-lg font-semibold"> {note.title}</h3>
+          <h3 className="truncate text-lg font-semibold"> {note.title}</h3>
           <span className="text-sm text-muted-foreground">
             {note.createdAt.toDateString()}
           </span>
