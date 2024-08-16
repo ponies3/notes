0c43605be70b2b58754ae31fa7f9a43c87b1c49a
avallejo <avallejofornes@gmail.com>
2024-08-16 12:09:58 +0200

    update note list with NoteCard

--- src/modules/notes/pages/list/list.tsx
+++ src/modules/notes/pages/list/list.tsx
@@ -1,26 +1,25 @@
 import { HydrateClient, api } from "@/trpc/server";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { CreateNewNoteDialog } from "../../components/createNoteButton";
 import Link from "next/link";
+import { NoteCard } from "../../components/noteCard";
 
 export async function NotesList() {
   const notes = await api.notes.geAll();
   return (
-    <HydrateClient>
-      <main>
-        <h1>Notes</h1>
-        <div>
+    <main>
+      <section className="container mx-auto px-4 py-12 md:px-6">
+        <div className="mb-8 flex items-center justify-between">
+          <h2 className="text-2xl font-bold">My Notes</h2>
           <CreateNewNoteDialog />
         </div>
-        <ul>
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {notes.map((note) => (
-            <li key={note.id}>
-              <Link href={`/notes/edit?id=${note.id}`} key={note.id}>
-                {note.title}
-              </Link>
-            </li>
+            <NoteCard key={note.id} note={note} />
           ))}
-        </ul>
-      </main>
-    </HydrateClient>
+        </div>
+      </section>
+    </main>
   );
 }
