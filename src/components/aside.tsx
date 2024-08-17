2da011bbcccc03ee8f1df9330ee566a189e98b79
avallejo <avallejofornes@gmail.com>
2024-08-17 12:58:38 +0200

    change aside title

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -1,4 +1,5 @@
 import { AsideNoteCardList } from "@/modules/notes/components/asideNoteCardList";
+import { History } from "lucide-react";
 import { Suspense } from "react";
 
 export async function Aside() {
@@ -6,7 +7,9 @@ export async function Aside() {
     <aside
       className={`mx-auto hidden h-screen w-72 flex-col gap-2 overflow-y-auto bg-zinc-100 px-4 py-12 md:px-6 lg:flex`}
     >
-      <h2 className="text-2xl font-bold">Modified notes history</h2>
+      <h2 className="flex items-center gap-1 text-base font-bold">
+        <History size={20} /> Modified notes history
+      </h2>
       <Suspense fallback={<div>Loading...</div>}>
         <AsideNoteCardList />
       </Suspense>
